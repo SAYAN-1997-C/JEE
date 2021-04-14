8fab4c2791369c1f699e8e935582ce6074e912bf
SAYAN-1997-C <71917167+SAYAN-1997-C@users.noreply.github.com>
2021-04-14 16:03:35 +0530

    Add files via upload

---
+++ Config_Context_Demo/src/master/servlet/EmployeeServe2.java
@@ -0,0 +1,36 @@
+package master.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployeeServe2
+ */
+public class EmployeeServe2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String id=request.getParameter("id");
+		String name=request.getParameter("name");
+		
+		out.println("Employee id : "+id);
+		out.println("<p>");
+		out.println("Employee name : "+name);
+		out.println("<p>");
+		out.println(getServletConfig().getInitParameter("Phone_no"));
+		out.println("<p>");
+		out.println(getServletContext().getInitParameter("Country"));
+	}
+
+}
